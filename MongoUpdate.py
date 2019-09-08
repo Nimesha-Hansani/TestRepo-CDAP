5bee5f9c5ef7a2a030bc0a4a03442b96306fc073
Nimesha-Hansani <nimeshaamarasingha@gmail.com>
2019-09-08 18:35:25 +0530

    Mongo

--- MongoUpdate.py
+++ MongoUpdate.py
@@ -1,13 +1,26 @@
+import os
+import glob
 import pymongo
+import re
+import keyword
 from pymongo import MongoClient
+from pymongo import InsertOne
+import datetime
 
+
+
+
+#DB Connection to the cognitive value collection
 
 myclient = MongoClient('localhost',27017)
 mydb = myclient["gCodexDB"]
-mycol =mydb["LinesOfCode"]
+mycol =mydb["cognitiveValues"]
 
 
 
-
-
-
+mycol.update_many({"Repository":"Nimesha-Hansani/TestRepo-CDAP",
+                         "Branches":{'$elemMatch':{"Branch Name":"master"}}},
+                          {'$push':{"Branches.$.Commits":{
+                                    "Commit Date":"2019-05-25",
+                                    "Commit Time":"18-12-11"
+                          }}})
